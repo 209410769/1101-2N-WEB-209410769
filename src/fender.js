418e2eee563ecdaa2d369d7736bb2ba94a70ec2a
209410769 <209410769@gms.tku.edu.tw>
2022-01-10 23:50:35 +0800

    modify fender.js&fender.css to V3

---
+++ src/fender.js
@@ -0,0 +1,220 @@
+import './fender.css'
+function app() {
+  return (
+      <div>
+    <header className="contact_wrap">
+    <div className="contact-header">
+        <div className="container">
+            <div className="row">
+                <div className="contact-header__container">
+                    <ul className="contact-header__list">
+                        <li><a href="#">FENDER.COM</a></li>
+                        <li><a href="#">PLAY</a></li>
+                        <li><a href="#">TUNE</a></li>
+                        <li><a href="#">BEGINNERS</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="header-banner"></div>
+    <div className="navbar-contact">
+        <div className="container">
+            <div className="row">
+                <div className="navbar-contact__container">
+                    <div className="navbar-menu-toggle">
+                        <a href="#"><img src="./img/fender_logo.png" alt="" width="80px" /></a>
+                    </div>
+                    <ul className="navbar-contact__list">
+                        <li>
+                            <a href="#">ELECTRICS
+                            <i className="fas fa-caret-down"/></a></li>
+                        <li><a href="#">ACOUSTASONIC<i className="fas fa-caret-down"></i></a></li>
+                        <li><a href="#">ACOUSTICS<i className="fas fa-caret-down"></i></a></li>
+                        <li><a href="#">BASSES<i className="fas fa-caret-down"></i></a></li>
+                        <li><a href="#">AMPS & AUDIO<i className="fas fa-caret-down"></i></a></li>
+                        <li><a href="#">PARTS<i className="fas fa-caret-down"></i></a></li>
+                        <li><a href="#">ACCESSORIES<i className="fas fa-caret-down"></i></a></li>
+                        <li><a href="#">LIFESTYLE<i className="fas fa-caret-down"></i></a></li>
+                        <li><a href="#">SQUIER<i className="fas fa-caret-down"></i></a></li>
+                    </ul>
+                    <div className="navbar-search-toggle">
+                        <a href="#"><i className="fas fa-search"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div className="menu-collection">
+    <div className="large-menu-item">
+        <div className="items">
+            <a href="#"><img src="./img/SCREAMDELICA_STRATOCASTER.jpg" alt="" width="100%" /></a>
+        </div>
+    </div>
+    <div className="middle-menu-item">
+        <div className="items">
+            <div className="item">
+                <a href="#"><img src="./img/2021_NEW_RELEASES.jpg" alt="" width="100%" /></a>
+            </div>
+            <div className="item">
+                <a href="#"><img src="./img/MUSTANG_MICRO.jpg" alt="" width="100%" /></a>
+            </div>
+        </div>
+    </div>
+    <div className="heading">
+        <hr/>
+        <h2>WHAT'S NEW</h2>
+    </div>
+    <div className="menu-item">
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/Acoustasonic_Player_Telecaster.png" alt="" width="180px" /></a>
+        </div>
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/30th_Anniversary_Screamadelica_Stratocaster.png" alt="" width="180px" /></a>
+        </div>
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/Player_Plus_Stratocaster.png" alt="" width="180px" /></a>
+        </div>
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/Player_Plus_Jazz_Bass.png" alt="" width="162px" /></a>
+        </div>
+    </div>
+    <div className="middle-menu-item">
+        <div className="popular-cat-heading">
+            <h3>FENDER FEED</h3>
+        </div>
+        <div className="items">
+            <div className="item">
+                <a href="#"><img src="./img/ANERICAN_PROFESSIONAL_II.jpg" alt="" width="100%" /></a>
+            </div>
+            <div className="item">
+                <a href="#"><img src="./img/THE_PLAYER_SERIES.jpg" alt="" width="100%" /></a>
+            </div>
+        </div>
+    </div>
+    <div className="middle-menu-item">
+        <div className="items">
+            <div className="item">
+                <a href="#"><img src="./img/THE_BILLIE-EILISH_UKULELE.jpg" alt="" width="100%" width="100%" /></a>
+            </div>
+            <div className="item">
+                <a href="#"><img src="./img/AMERICAN_ULTRA_LUXE.jpg" alt="" width="100%" /></a>
+            </div>
+        </div>
+    </div>
+    <div className="heading">
+        <hr/>
+        <h2>ACCESSORIES</h2>
+    </div>
+    <div className="menu-item">
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/Pro_Glow_in_the_Dark_Cables.png" alt="" width="100%" /></a>
+        </div>
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/351_Guitar_Wall_Hanger.png" alt="" width="120px" /></a>
+        </div>
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/Fender_x_Kyser_Quick_Change_Electric_Guitar_Capo.png" alt=""
+                    width="100%" /></a>
+        </div>
+        <div className="item">
+            <div className="new-tag-true">new</div>
+            <a href="#"><img src="./img/Neon_Monogrammed_Strap.png" alt="" width="100%" /></a>
+        </div>
+    </div>
+</div>
+<footer>
+    <div className="footer-container row">
+        <div className="footer-column-logo">
+            <a href="./index.html" className="global-footer__logo">
+                <img src="./img/fender-footer.png" alt="" width="141px" />
+            </a>
+        </div>
+        <div className="footer-column-content">
+            <div className="global-footer__nav">
+                <div className="row">
+                    <ul>
+                        <h6 className="uppercase">Products</h6>
+                        <li><a href="#">Electric Guitars</a></li>
+                        <li><a href="#">Stratocaster</a></li>
+                        <li><a href="#">Telecaster</a></li>
+                        <li><a href="#">Guitar Amplifiers</a></li>
+                        <li><a href="#">Basses</a></li>
+                        <li><a href="#">Acoustic Guitars</a></li>
+                        <li><a href="#">Fender Custom Shop</a></li>
+                    </ul>
+                    <ul>
+                        <h6 className="uppercase">Company</h6>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#" target="_blank">Newsroom</a></li>
+                    </ul>
+                    <ul>
+                        <h6 className="uppercase">More from Fender</h6>
+                        <li><a href="#">Online Guitar Tuner</a></li>
+                        <li>
+                            <a href="#" alt="Tuner App" title="Tuner App">Tuner App</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h6 className="uppercase">Customer Service</h6>
+                        <li><a href="#">Support</a></li>
+                        <li>
+                            <a href="#">Find a Dealer</a>
+                        </li>
+
+                        <li>
+                            <a href="#" target="_blank">Warranty</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">Manuals</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">Register Your Fender</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h6 className="uppercase">Account</h6>
+                        <li><a href="#">My Account</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-social-countryselector">
+                <div className="footer-countryselector">
+                    <div className="language-select">
+                        <div className="country-language-selector">
+                            <span></span>
+                            <a data-toggle="modal" data-target="#" role="button" href="#">
+                                <span className="flag-intl"></span>International
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="global-footer__social">
+                    <div className="content-asset">
+                        <div className="social-media">
+                            <a href="#"><i className="fab fa-facebook-square"></i></a>
+                            <a href="#"><i className="fab fa-twitter"></i></a>
+                            <a href="#"><i className="fab fa-instagram"></i></a>
+                            <a href="#"><i className="fab fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</footer>
+</div>
+  );
+}
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './midproj';
+import App from './fender';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
